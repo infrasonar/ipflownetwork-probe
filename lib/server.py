1c906cc016a2abdf3db43f01b30d0c922a05d3d9
Koos85 <koosjoosten@gmail.com>
2024-09-30 12:29:26 +0200

    FORWARD_PORTS env var

--- lib/server.py
+++ lib/server.py
@@ -8,12 +8,21 @@ from lib.ipflow.parser_v5 import on_packet_v5
 from .state import subscriptions
 
 LISTEN_PORT = int(os.getenv('LISTEN_PORT', '2055'))
+FORWARD_PORTS = os.getenv('FORWARD_PORTS')
+FORWARD = [
+    ('127.0.0.1', pt)
+    for pt in map(int, FORWARD_PORTS.split(','))
+] if FORWARD_PORTS else []
+assert all(0 < pt < 65536 and pt != LISTEN_PORT for _, pt in FORWARD)
 
 COMMON_HEADER_FMT = '>HHL'
 COMMON_HEADER_SZ = 8
 
 
 class ServerProtocol(asyncio.Protocol):
+
+    def connection_made(self, transport):
+        self.transport = transport
 
     def datagram_received(self, data, addr):
         if len(data) < COMMON_HEADER_SZ:
@@ -28,6 +37,12 @@ class ServerProtocol(asyncio.Protocol):
         if version not in (5, 9, 10):
             logging.warning('unsupported netflow version')
             return
+
+        # TODO
+        # should this be done ahead of the above checks?
+        # assume no exceptions?
+        for dest in FORWARD:
+            self.transport.sendto(data, dest)
 
         # v5 has no templates so could be ignored when no checks are listening
         if version == 5 and not subscriptions:
@@ -48,10 +63,20 @@ class ServerProtocol(asyncio.Protocol):
 def start_server(loop: asyncio.AbstractEventLoop):
     logging.info('Starting UDP server')
 
-    transport, protocol = loop.run_until_complete(
-        loop.create_datagram_endpoint(
-            ServerProtocol,
-            local_addr=('0.0.0.0', LISTEN_PORT))
-    )
-
-    return transport, protocol
+    try:
+        loop.run_until_complete(
+            loop.create_datagram_endpoint(
+                ServerProtocol,
+                local_addr=('0.0.0.0', LISTEN_PORT))
+        )
+    except OSError:
+        logging.critical(
+            f'Port {LISTEN_PORT} is already in use. Most likely the '
+            '`ipflow-probe` is using it. Configure the `ipflownetwork-probe` '
+            f'to listen to port {LISTEN_PORT + 1} and set the '
+            '`ipflow-probe` to forward the traffic there. See the '
+            '`LISTEN_PORT` and `FORWARD_PORTS` environment variable or use '
+            'the appliance manager '
+            '(https://github.com/infrasonar/appliance-manager).'
+        )
+        exit(1)
